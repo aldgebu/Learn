e493f254e82e49f57099c34aef27f0ccdb63fa2d
aldgebu <aldgebu@gmail.com>
2022-12-26 19:38:24 +0400

    progressBar

---
+++ progressBar/progressBar.js
@@ -0,0 +1,28 @@
+const cliProgress = require('cli-progress');
+const fs = require('fs');
+
+// progressBar
+const load = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+
+// file size
+let stats = fs.statSync('./input');
+let sizeinby = stats.size;
+let chars = sizeinby;
+
+load.start(chars, 0);
+
+let sum = 0;
+const read = () => {
+   const readStream = fs.createReadStream('input', {highWaterMark : 16});
+
+    readStream.on('data', (chunk) => {
+        sum += chunk.length;
+        load.update(sum);
+    })
+
+    readStream.on('end', () => {
+        load.stop();
+    })
+}
+
+read();
