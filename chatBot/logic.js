98a887ba946cac5f4f1946a3547165663ed86ddd
aldgebu <aldgebu@gmail.com>
2023-01-04 11:47:27 +0400

    ra unda???

--- chatBot/logic.js
+++ chatBot/logic.js
@@ -2,34 +2,26 @@ const telegramBot = require('node-telegram-bot-api');
 require('dotenv').config();
 
 class bot{
-    constructor() {
-        const TOKEN = process.env.TOKEN;
-        this.Bot = new telegramBot(TOKEN, {polling : true});
-    };
-
-    about(message){
-        console.log(1);/*
-        let chat_id = message.from.id;
+    help(){
         const ans = 'I have three commands for this time, :// \n' +
             '1) help, witch you used right now. \n' +
             '2) link, with this command I will give you information about my github and facebook\n' +
             '3) about, in this command I will tell you some new informations about me, magari kargmotynuli vinme var ise :)';
-        Bot.sendMessage(chat_id, ans);*/
-    };
-
-    help(message){
-        let chat_id = message.from.id;
-        Bot.sendMessage(chat_id, 'Vinnn xaaar Seeen bratuxaa chemi pirovnebit ro interesdebi????');
+        return ans;
     }
 
-    link(message){
-        let chat_id = message.from.id;
+    link(){
         const ans = 'Let me show my Facebook and Github links: \n' +
             'My facebook: https://www.facebook.com/aldgebu/\n' +
             'My Github: https://github.com/aldgebu';
-        Bot.sendMessage(chat_id, ans);
+
+        return ans;
     }
 
+    about(){
+        const ans = "vin xarr rom chemi pirovnebit interesdebi brauxaa?";
+        return ans;
+    }
 }
 
 module.exports = bot;
--- chatBot/main.js
+++ chatBot/main.js
@@ -1,6 +1,19 @@
-const botLogic = require('./logic');
-const bot = new botLogic();
+const telegramBot = require('node-telegram-bot-api');
+require('dotenv').config();
+const TOKEN = process.env.TOKEN;
+const Bot = new telegramBot(TOKEN, {polling : true});
 
-bot.Bot.onText(/^\/about/, message => {
-    bot.about(message);
+const bot = require('./logic.js');
+const telegrambot = new bot;
+
+Bot.onText(/^\/about/, (message) => {
+    Bot.sendMessage(telegrambot.about());
+})
+
+Bot.onText(/^\/link/, (message) => {
+    Bot.sendMessage(telegrambot.link());
+})
+
+Bot.onText(/^\/help/, (message) => {
+    Bot.sendMessage(telegrambot.help());
 })
