754583fc7477d29492edc59c442933fa720af7d5
aldgebu <aldgebu@gmail.com>
2023-01-04 11:58:26 +0400

    mgoni eshvela

--- chatBot/main.js
+++ chatBot/main.js
@@ -4,16 +4,19 @@ const TOKEN = process.env.TOKEN;
 const Bot = new telegramBot(TOKEN, {polling : true});
 
 const bot = require('./logic.js');
-const telegrambot = new bot;
+const telegrambot = new bot();
 
 Bot.onText(/^\/about/, (message) => {
-    Bot.sendMessage(telegrambot.about());
+    let chat_id = message.from.id;
+    Bot.sendMessage(chat_id, telegrambot.about());
 })
 
 Bot.onText(/^\/link/, (message) => {
-    Bot.sendMessage(telegrambot.link());
+    let chat_id = message.from.id;
+    Bot.sendMessage(chat_id, telegrambot.link());
 })
 
 Bot.onText(/^\/help/, (message) => {
-    Bot.sendMessage(telegrambot.help());
+    let chat_id = message.from.id;
+    Bot.sendMessage(chat_id, telegrambot.help());
 })
